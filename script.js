07bdc717cb5a4d5aac871e62e161b928dbb15a96
exlymex <94551924+exlymex@users.noreply.github.com>
2022-01-11 16:50:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// person = {
+//         firstName:'Ivan',
+//         secondName:'Ivanov',
+//         showData(){
+//             console.log(` name : ${this.firstName}, surname : ${this.secondName}`)
+//         }
+// }
+// const newPerson = Object.assign({}, person)
+// newPerson.firstName = 'Petro';
+// newPerson.secondName = 'Petriv'
+// console.log(person.showData())
+// console.log(newPerson.showData())
+
+// myMath = {
+//     a:5,
+//     b:2,
+//     sum(){
+//         return this.a + this.b
+//     },
+//     multiplication(){
+//         return this.a * this.b
+//     },
+//     division(){
+//         return this.a / this.b
+//     },
+//     subtraction(){
+//         return this.a - this.b
+//     }
+// }
+// myMath.sum()
+// console.log(myMath.sum())
+// console.log(myMath.multiplication())
+// console.log(myMath.division())
+// console.log(myMath.subtraction())
+
+// function Urus(stName,stSurname) {
+//     this.stName = stName;
+//     this.stSurname = stSurname;
+// }
+// const st1 = new Urus('Pasha','Nastia')
+// console.log(st1)
+
